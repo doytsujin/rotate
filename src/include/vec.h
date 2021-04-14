18f2f14e67747b09eb2602a3181e980e60111601
Airbus5717 <55999379+Airbus5717@users.noreply.github.com>
2021-04-14 12:39:51 +0300

    Update vec.h

--- src/include/vec.h
+++ src/include/vec.h
@@ -1,21 +1,5 @@
-// vec.h: A C macro dynamic-array library.
-// Copyright (C) 2021 Ethan Uppal
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-#ifndef vector_h
-#define vector_h
+#ifndef VECTOR_H
+#define VECTOR_H
 
 #include <stddef.h>
 #include <stdlib.h>
